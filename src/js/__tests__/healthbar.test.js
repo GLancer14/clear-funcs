750530ec44ad3a70fd3550a2bc3b366584e88cf5
unknown <181259602+GLancer14@users.noreply.github.com>
2025-06-08 11:28:56 +0400

    Codestyle corrected

--- src/js/__tests__/healthbar.test.js
+++ src/js/__tests__/healthbar.test.js
@@ -4,7 +4,7 @@ test.each([
   ['healthy', {name: 'Маг', health: 90}, 'healthy'],
   ['wounded', {name: 'Маг', health: 35}, 'wounded'],
   ['critical', {name: 'Маг', health: 10}, 'critical'],
-])('testing health condition function for %s condition when stats is %o', (status, inputValue, expectedValue) => {
+])('testing health condition function for %s condition when stats is %o', (_, inputValue, expectedValue) => {
   const outputValue = defineHealthCondition(inputValue);
   expect(outputValue).toBe(expectedValue);
-})
+});
